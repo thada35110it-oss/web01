4b0e08eb75ce68d798087582db7cf78115471f4b
thada35110it-oss <thada35110it@gmail.com>
2026-06-29 12:10:34 +0700

    Add files via upload

---
+++ ws003 029/vas/script.js
@@ -0,0 +1,10 @@
+for(i=1;i<=5;i++){
+    document.write("<h",i,"> มหาวิทยาลัยราชภัฏนครปฐม </h",i,">");
+}
+
+for(i=0; i<10; i++){
+    for(j=0; j<=i; j++){
+        document.write("<img src='img/Courage.jpg' width=35>");
+    }
+    document.write("<br>");
+}
